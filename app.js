f7400468a8b7e17b8fdd4f3cb9b11c8c606291c6
LayanaBaba <leana_baba@yahoo.com>
2021-03-18 14:32:38 +0200

    lab04 finishing

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
 var username= prompt ("what's your name?");
-document.write ('<h5>'+username+'</h5>')
+document.write ('<h5>'+"Hello ",username+'</h5>')
+
+
+var love= prompt ("Do you love flowers?");
+if (love=='yes'){document.write('<h6>'+"That's Great"+'</h6>');}
+
+
+
+
+var favFlo= prompt ("What's your favourit flower?")
+
+if (favFlo=='Calenduld'){document.write('<img src="./cal.jpg"/>');}
+else if (favFlo=='Diascia') {document.write('<img src="./dia.jpg"/>');}
+else {alert('Choose between Calenduld and Diascia');}
+
+
+
+
